921db5b4e1a5109891a8b73397d13a6632df200c
tygris <youssif6920@gmail.com>
2025-11-25 23:41:24 -0700

    removed back button from StatusBar

--- App.jsx
+++ App.jsx
@@ -1,60 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import HomeScreen from './src/screens/HomeScreens';
 import BudgetScreen from './src/screens/BudgetScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+
 import { styles } from './src/ui/shared/styles';
+import useTheme from './src/ui/shared/themeSelect';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
+  const { theme, themeName } = useTheme();
 
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <Stack.Navigator initialRouteName="Home">
+        
+        
+        <StatusBar
+          backgroundColor={theme.card}
+          barStyle={themeName === "dark" ? "light-content" : "dark-content"}
+        />
+
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.card,   
+            },
+            headerTitleStyle: {
+              color: theme.text,
+              fontSize: 20,
+              fontWeight: 'bold',
+            },
+            headerTintColor: theme.text,      
+            headerShadowVisible: false,        
+          }}
+        >
+          
           <Stack.Screen 
-            style
             name="Home" 
             component={HomeScreen}
-            options={{
-              title: 'CashLens',
-              headerTitleStyle: styles.title,
-            }}
+             options={{
+             headerBackVisible: false, 
+            title: "CashLens",
+  }}
           />
+
           <Stack.Screen 
             name="Budget" 
             component={BudgetScreen}
-            options={{
-              title: 'CashLens',
-              headerTitleStyle: styles.title
-            }}
-          />
-          <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
+           options={{
+             headerBackVisible: false, 
             title: "CashLens",
-            headerTitleStyle: styles.title
-          }}
+  }}
           />
+
+          <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+           options={{
+             headerBackVisible: false, 
+            title: "CashLens",
+  }}
+          />
+
+          {/* <Stack.Screen 
+            name="Settings"
+            component={SettingsScreen}
+            
+          /> */}
         </Stack.Navigator>
+
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-
 
 export default App;
